1511b707eed49a0319204429913e265405542937
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2019-05-14 11:04:45 +0530

    change profile items

--- Modules/Admin/Resources/views/staff_admin/components/head.blade.php
+++ Modules/Admin/Resources/views/staff_admin/components/head.blade.php
@@ -113,22 +113,9 @@
                                 </a>
                             </div>
                         </div>                        
-                        //condoer
-                    <div class="user-area dropdown float-right">
-                        <a href="#" class="dropdown-toggle active" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <img class="user-avatar rounded-circle" src="{{ (Theme::url('public/HousePan/images/admin.jpg')) }}" alt="User Avatar">
-                        </a>
+        
+        @include('admin::staff_admin.components.profile_items')
 
-                        <div class="user-menu dropdown-menu">
-                            <a class="nav-link" href="#"><i class="fa fa- user"></i>My Profile</a>
-
-                            <a class="nav-link" href="#"><i class="fa fa- user"></i>Notifications <span class="count">13</span></a>
-
-                            <a class="nav-link" href="#"><i class="fa fa -cog"></i>Settings</a>
-
-                            <a class="nav-link" href="#"><i class="fa fa-power -off"></i>Logout</a>
-                        </div>
-                    </div>
 
                 </div>
             </div>
