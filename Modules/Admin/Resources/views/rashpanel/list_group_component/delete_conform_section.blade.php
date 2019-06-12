c8e0fcb6b47c8c891bb0f3a13c172013fee2d4a7
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2019-06-12 11:33:53 +0530

    Delete Section

---
+++ Modules/Admin/Resources/views/rashpanel/list_group_component/delete_conform_section.blade.php
@@ -0,0 +1,20 @@
+<div id="myModal" class="modal fade" role="dialog">
+    <div class="modal-dialog">      
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Delete Section</h4>
+        </div>
+        <div class="modal-body">
+              <p>Are you really want to know delete this section?</p>                 
+        </div>
+        <div class="modal-footer">
+            <button type="submit" value="Validate!" class="btn btn-primary">Save</button>
+        </form>
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+  
+    </div>
+  </div>
