6f114c861a303ec601d70e34d6c65b77b4d69320
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2019-06-18 14:48:27 +0530

    section course_home

---
+++ Themes/Storefront/views/public/home_land/components/course_list.blade.php
@@ -0,0 +1,201 @@
+<!-- Course -->
+<section class="courses section-padding-large bg-parten section-bg-overlay">
+        <div class="container">
+            <div class="section-title-white section-title-center">
+                <h2>Popular Courses</h2>
+                <p class="title-desc">
+                    Tempus egestas sed sed risus pretium quam vulputate. Sit amet est placerat in id nibh tortor egestas erat imperdiet sed.
+                </p>
+            </div>
+            <div class="courses-content featured-course-slider">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+                        <article class="item wow fadeIn" data-wow-delay="0.1s">
+                            <div class="item-thumb">
+                                <a href="single-course.html">
+                                    <img src="{{ (Theme::url('public/visual_style/images/home1-course-1.jpg')) }}" alt="">
+                                    <span class="overlay"></span>
+                                </a>
+                                <div class="feadtured-course-small">
+                                    <a href="#">
+                                        <img src="{{ (Theme::url('public/visual_style/images/course-small-1.jpg')) }}" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="info">
+                                <a href="#" class="teacher">Steven Boyd</a>
+                                <h3 class="title">
+                                    <a href="single-course.html">Basic: Web Developer</a>
+                                </h3>
+                                <p class="course-desc">
+                                    Morbi tristique senectus et ne malesuada fames ac turpis. Tempus egestas sed sed risus pretium quam vulputate.
+                                </p>
+                                <div class="desc display-flex">
+                                    <div class="comments-students">
+                                        <a href="#" class="comments"><i class="fas fa-user"></i>20 Students</a>
+                                        <a href="#" class="students"><i class="fas fa-book"></i>05 Lessons</a>
+                                    </div>
+                                    <span class="price free">Free</span>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+                        <article class="item wow fadeIn" data-wow-delay="0.3s">
+                            <div class="item-thumb">
+                                <a href="single-course.html">
+                                    <img src="{{ (Theme::url('public/visual_style/images/home1-course-2.jpg')) }}" alt="">
+                                    <span class="overlay"></span>
+                                </a>
+                                <div class="feadtured-course-small">
+                                    <a href="#">
+                                        <img src="{{ (Theme::url('public/visual_style/images/course-small-2.jpg')) }}" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="info">
+                                <a href="#" class="teacher">Andrea Russell</a>
+                                <h3 class="title">
+                                    <a href="single-course.html">Basic WordPress</a>
+                                </h3>
+                                <p class="course-desc">
+                                    Ullamcorper eget nulla facilisi dignissim diam quis enim lobortis. Ali malesuada bibendum arcu vitae.
+                                </p>
+                                <div class="desc display-flex">
+                                    <div class="comments-students">
+                                        <a href="#" class="comments"><i class="fas fa-user"></i>08 Students</a>
+                                        <a href="#" class="students"><i class="fas fa-book"></i>08 Lessons</a>
+                                    </div>
+                                    <span class="price notfree">$40.00</span>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+                        <article class="item wow fadeIn" data-wow-delay="0.5s">
+                            <div class="item-thumb">
+                                <a href="single-course.html">
+                                    <img src="{{ (Theme::url('public/visual_style/images/home1-course-3.jpg')) }}" alt="">
+                                    <span class="overlay"></span>
+                                </a>
+                                <div class="feadtured-course-small">
+                                    <a href="#">
+                                        <img src="{{ (Theme::url('public/visual_style/images/course-small-2.jpg')) }}" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="info">
+                                <a href="#" class="teacher">Andrea Russell</a>
+                                <h3 class="title">
+                                    <a href="single-course.html">Web Design For Usability</a>
+                                </h3>
+                                <p class="course-desc">
+                                    Mauris vitae ultricies leo inte malesuada. Proin sagittis nisl rhoncus matt rhoncu urna amet est placerat egestas.
+                                </p>
+                                <div class="desc display-flex">
+                                    <div class="comments-students">
+                                        <a href="#" class="comments"><i class="fas fa-user"></i>25 Students</a>
+                                        <a href="#" class="students"><i class="fas fa-book"></i>10 Lessons</a>
+                                    </div>
+                                    <span class="price notfree">$50.00</span>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+                        <article class="item">
+                            <div class="item-thumb">
+                                <a href="single-course.html">
+                                    <img src="images/home1-course-4.jpg" alt="">
+                                    <span class="overlay"></span>
+                                </a>
+                                <div class="feadtured-course-small">
+                                    <a href="#">
+                                        <img src="images/course-small-3.jpg" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="info">
+                                <a href="#" class="teacher">Lucas Coleman</a>
+                                <h3 class="title">
+                                    <a href="single-course.html">Advanced: UI Design</a>
+                                </h3>
+                                <p class="course-desc">
+                                    Diam quam nulla porttitor massa neque aliquam vestibulum. Ullamcorper eget nulla facilisi consectetur.
+                                </p>
+                                <div class="desc display-flex">
+                                    <div class="comments-students">
+                                        <a href="#" class="comments"><i class="fas fa-user"></i>35 Students</a>
+                                        <a href="#" class="students"><i class="fas fa-book"></i>20 Lessons</a>
+                                    </div>
+                                    <span class="price notfree">$65.00</span>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+                        <article class="item">
+                            <div class="item-thumb">
+                                <a href="single-course.html">
+                                    <img src="images/home1-course-5.jpg" alt="">
+                                    <span class="overlay"></span>
+                                </a>
+                                <div class="feadtured-course-small">
+                                    <a href="#">
+                                        <img src="images/course-small-2.jpg" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="info">
+                                <a href="#" class="teacher">Andrea Russell</a>
+                                <h3 class="title">
+                                    <a href="single-course.html">Graphic Design</a>
+                                </h3>
+                                <p class="course-desc">
+                                    Odio tempor orci dapibus ultrice iaculis. Tincidunt eget nullam non nisi est sit amet pretium lectus.
+                                </p>
+                                <div class="desc display-flex">
+                                    <div class="comments-students">
+                                        <a href="#" class="comments"><i class="fas fa-user"></i>26 Students</a>
+                                        <a href="#" class="students"><i class="fas fa-book"></i>05 Lessons</a>
+                                    </div>
+                                    <span class="price free">Free</span>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+                        <article class="item">
+                            <div class="item-thumb">
+                                <a href="single-course.html">
+                                    <img src="images/home1-course-6.jpg" alt="">
+                                    <span class="overlay"></span>
+                                </a>
+                                <div class="feadtured-course-small">
+                                    <a href="#">
+                                        <img src="images/course-small-1.jpg" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="info">
+                                <a href="#" class="teacher">Steven Boyd</a>
+                                <h3 class="title">
+                                    <a href="single-course.html">Design System</a>
+                                </h3>
+                                <p class="course-desc">
+                                    Gravida in fermentum et sollicitudi orci phasellus. Diam quam nulla portt masa id neque aliquam vestibulum.
+                                </p>
+                                <div class="desc display-flex">
+                                    <div class="comments-students">
+                                        <a href="#" class="comments"><i class="fas fa-user"></i>50 Students</a>
+                                        <a href="#" class="students"><i class="fas fa-book"></i>18 Lessons</a>
+                                    </div>
+                                    <span class="price notfree">$45.00</span>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
