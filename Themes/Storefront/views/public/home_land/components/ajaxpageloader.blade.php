c0b8264261a74ade51935a8d76ec51598650bd12
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2019-06-18 14:40:52 +0530

    micropis_ajaxloader_seperation

---
+++ Themes/Storefront/views/public/home_land/components/ajaxpageloader.blade.php
@@ -0,0 +1,10 @@
+<body>
+
+        <!-- page load-->
+        <div class="images-preloader">
+            <div id="preloader_1 spinner1" class="spinner1 rectangle-bounce">
+                <div class="double-bounce1"></div>
+                <div class="double-bounce2"></div>
+            </div>
+        </div>
+    
