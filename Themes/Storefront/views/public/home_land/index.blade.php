bed62c1bff31fa593e3192d172eaa05eb8cb7d13
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2019-06-18 14:40:19 +0530

    micropisi

--- Themes/Storefront/views/public/home_land/index.blade.php
+++ Themes/Storefront/views/public/home_land/index.blade.php
@@ -2,40 +2,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home1 SmartEdu</title>
-
-    <!-- Bootstrap -->
-    
-    <link rel='stylesheet' href='{{ (Theme::url('public/visual_style/vendor/jquery-ui/jquery-ui.min.css')) }}'>
-    <link rel="stylesheet" href="{{ (Theme::url('public/visual_style/vendor/bootstrap/css/bootstrap.min.css')) }}">
-
-    <!-- Font Icon -->
-    <link rel="stylesheet" href="{{ (Theme::url('public/visual_style/fonts/line-awesome/css/line-awesome.min.css')) }}">
-    <link rel="stylesheet" href="{{ (Theme::url('public/visual_style/fonts/font-awesome/css/font-awesome.min.css')) }}">
-    <link rel="stylesheet" href="{{ (Theme::url('public/visual_style/fonts/font-awesome-5/css/fontawesome-all.min.css')) }}">
-
-    <!-- Revolution slider -->
-    <link rel="stylesheet" href="{{ (Theme::url('public/visual_style/vendor/revolution/settings.css')) }}">
-    <link rel="stylesheet" href="{{ (Theme::url('public/visual_style/vendor/revolution/layers.css')) }}">
-    <link rel="stylesheet" href="{{ (Theme::url('public/visual_style/vendor/revolution/navigation.css')) }}">
-    <link rel="stylesheet" href="{{ (Theme::url('public/visual_style/vendor/revolution/settings-source.css')) }}">
-
-    <!-- Vendor CSS -->
-    <link rel="stylesheet" href="{{ (Theme::url('public/visual_style/vendor/css-hamburgers/dist/hamburgers.min.css')) }}">
-    <link rel="stylesheet" href="{{ (Theme::url('public/visual_style/vendor/slick/slick-theme.css')) }}">
-    <link rel="stylesheet" href="{{ (Theme::url('public/visual_style/vendor/slick/slick.css')) }}">
-    <link rel="stylesheet" href="{{ (Theme::url('public/visual_style/vendor/fancybox/dist/jquery.fancybox.min.css')) }}">
-    <link rel='stylesheet' href='{{ (Theme::url('public/visual_style/vendor/fullcalendar/fullcalendar.cs')) }}' />
-    <link rel='stylesheet' href='{{ (Theme::url('public/visual_style/vendor/animate/animate.css')) }}' />
-
-    <!-- Main CSS File -->
-    <link href="{{ (Theme::url('public/visual_style/css/style.css')) }}" rel="stylesheet">
-    <link rel="shortcut icon" href="{{ (Theme::url('public/visual_style/favicon.png')) }}">
-</head>
+@include('public.home_land.components.head')
 
 <body>
 
