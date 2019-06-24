470b5fb02a364aeb61d9504531b27e0bd18f2545
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2019-06-24 10:18:09 +0530

    lino

--- Themes/Storefront/views/public/home_land/course_list.blade.php
+++ Themes/Storefront/views/public/home_land/course_list.blade.php
@@ -30,25 +30,21 @@
         <div class="container">
             <div class="courses-content">
                 <div class="row">
-                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
+                   @foreach($coursedata as $item ) 
+                   <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
                         <article class="item">
                             <div class="item-thumb">
                                 <a href="single-course.html">
-                                    <img src="images/home2-course-1.jpg" alt="Steven Boyd">
-                                </a>
-                                <div class="feadtured-course-small">
-                                    <a href="#">
-                                        <img src="images/course-small-1.jpg" alt="Basic: Web Developer">
-                                    </a>
-                                </div>
+                                    <img src="{{ url('/') }}/get_data_reqst_java/{{ $item->course_id }}" style="height: 200px;width: -webkit-fill-available;" alt="Steven Boyd">
+                                </a>                               
                             </div>
                             <div class="info">
                                 <a href="#" class="teacher">Steven Boyd</a>
                                 <h3 class="title">
-                                    <a href="single-course.html">Basic: Web Developer</a>
+                                    <a href="single-course.html">{{ $item->course_title }}</a>
                                 </h3>
                                 <p class="course-desc">
-                                    Morbi tristique senectus et ne malesuada fames ac turpis. Tempus egestas sed.
+                                    {{ $item->course_discription }}
                                 </p>
                                 <div class="desc display-flex">
                                     <div class="comments-students">
@@ -59,159 +55,10 @@
                                 </div>
                             </div>
                         </article>
-                    </div>
-                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                        <article class="item">
-                            <div class="item-thumb">
-                                <a href="single-course.html">
-                                    <img src="images/home2-course-2.jpg" alt="Content Marketin">
-                                </a>
-                                <div class="feadtured-course-small">
-                                    <a href="#">
-                                        <img src="images/course-small-2.jpg" alt="Andrea Russell">
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="info">
-                                <a href="#" class="teacher">Andrea Russell</a>
-                                <h3 class="title">
-                                    <a href="single-course.html">Content Marketing</a>
-                                </h3>
-                                <p class="course-desc">
-                                    Est ante in nibh mauris cursus molestie sit amet volutpat.
-                                </p>
-                                <div class="desc display-flex">
-                                    <div class="comments-students">
-                                        <a href="#" class="comments"><i class="fas fa-user"></i>35 Students</a>
-                                        <a href="#" class="students"><i class="fas fa-book"></i>08 Lessons</a>
-                                    </div>
-                                    <span class="price notfree">$24.00</span>
-                                </div>
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                        <article class="item">
-                            <div class="item-thumb">
-                                <a href="single-course.html">
-                                    <img src="images/home2-course-3.jpg" alt="SEO Training Course">
-                                    <span class="overlay"></span>
-                                </a>
-                                <div class="feadtured-course-small">
-                                    <a href="#">
-                                        <img src="images/course-small-2.jpg" alt="Andrea Russell">
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="info">
-                                <a href="#" class="teacher">Andrea Russell</a>
-                                <h3 class="title">
-                                    <a href="single-course.html">SEO Training Course</a>
-                                </h3>
-                                <p class="course-desc">
-                                    Fermentum posuere urna nec tincidunt praesent semper feugiat nibh.
-                                </p>
-                                <div class="desc display-flex">
-                                    <div class="comments-students">
-                                        <a href="#" class="comments"><i class="fas fa-user"></i>24 Students</a>
-                                        <a href="#" class="students"><i class="fas fa-book"></i>30 Lessons</a>
-                                    </div>
-                                    <span class="price notfree">$40.00</span>
-                                </div>
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                        <article class="item">
-                            <div class="item-thumb">
-                                <a href="single-course.html">
-                                    <img src="images/courses-14.jpg" alt="Advanced: UI Design">
-                                </a>
-                                <div class="feadtured-course-small">
-                                    <a href="#">
-                                        <img src="images/course-small-3.jpg" alt="Lucas Coleman">
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="info">
-                                <a href="#" class="teacher">Lucas Coleman</a>
-                                <h3 class="title">
-                                    <a href="single-course.html">Advanced: UI Design</a>
-                                </h3>
-                                <p class="course-desc">
-                                    Lobortis mattis aliquam faucibus purus in massa tempor nec.
-                                </p>
-                                <div class="desc display-flex">
-                                    <div class="comments-students">
-                                        <a href="#" class="comments"><i class="fas fa-user"></i>22 Students</a>
-                                        <a href="#" class="students"><i class="fas fa-book"></i>12 Lessons</a>
-                                    </div>
-                                    <span class="price notfree">$65.00</span>
-                                </div>
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                        <article class="item">
-                            <div class="item-thumb">
-                                <a href="single-course.html">
-                                    <img src="images/courses-13.jpg" alt="Design System">
-                                    <span class="overlay"></span>
-                                </a>
-                                <div class="feadtured-course-small">
-                                    <a href="#">
-                                        <img src="images/course-small-1.jpg" alt="Steven Boyd">
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="info">
-                                <a href="#" class="teacher">Steven Boyd</a>
-                                <h3 class="title">
-                                    <a href="single-course.html">Design System</a>
-                                </h3>
-                                <p class="course-desc">
-                                    Pharetra et ultrices neque ornare aenean euismod elementum.
-                                </p>
-                                <div class="desc display-flex">
-                                    <div class="comments-students">
-                                        <a href="#" class="comments"><i class="fas fa-user"></i>20 Students</a>
-                                        <a href="#" class="students"><i class="fas fa-book"></i>16 Lessons</a>
-                                    </div>
-                                    <span class="price free">Free</span>
-                                </div>
-                            </div>
-                        </article>
-                    </div>
-                    <div class="col-xl-4 col-lg-4 col-md-6 col-sm-12 col-12">
-                        <article class="item">
-                            <div class="item-thumb">
-                                <a href="single-course.html">
-                                    <img src="images/courses-12.jpg" alt="Graphic Design">
-                                </a>
-                                <div class="feadtured-course-small">
-                                    <a href="#">
-                                        <img src="images/home3-tutor-1.jpg" alt="Melissa Peterson">
-                                    </a>
-                                </div>
-                            </div>
-                            <div class="info">
-                                <a href="#" class="teacher">Melissa Peterson</a>
-                                <h3 class="title">
-                                    <a href="single-course.html">Graphic Design</a>
-                                </h3>
-                                <p class="course-desc">
-                                    Quam adipiscing vitae proin sagittis nisl rhoncus mattis rhoncus.
-                                </p>
-                                <div class="desc display-flex">
-                                    <div class="comments-students">
-                                        <a href="#" class="comments"><i class="fas fa-user"></i>30 Students</a>
-                                        <a href="#" class="students"><i class="fas fa-book"></i>21 Lessons</a>
-                                    </div>
-                                    <span class="price notfree">$20.00</span>
-                                </div>
-                            </div>
-                        </article>
-                    </div>
+                    </div> 
+
+                   @endforeach                  
+                                     
                 </div>
             </div>
         </div>
