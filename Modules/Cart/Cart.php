571137a42dac0255e120d765432fa1bf11bf6208
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2019-06-28 15:30:04 +0530

    Bmach Quary

--- Modules/Cart/Cart.php
+++ Modules/Cart/Cart.php
@@ -142,9 +142,11 @@ class Cart extends DarryldecodeCart
            // $cartItem->product->decrement('qty', $cartItem->qty);
            // $query = DB::table('sol_packitem')->select('product_id');
            $contra = $cartItem->product->id;
-
+           
+            
            $get_course_details = DB::table('course_list')->where('reccomandproduct_id', $contra)->first();   
            
+
             
            $prod = DB::table('products')->where('id', $contra)->first();          
 
